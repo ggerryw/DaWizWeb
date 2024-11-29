53dcd8030fd131663d388c92b49bc05ce8cf908d
ggerryw <104268807+ggerryw@users.noreply.github.com>
2024-11-29 11:28:22 -0500

    tpgpppgData

--- JSON/tpgpppgData.js
+++ JSON/tpgpppgData.js
@@ -1,11 +1,11 @@
-{
-    "tpgpppg": {
+const tpgpppgData = {
+  "tpgpppg": {
       "categories": ["Luke", "Gerry", "Luc", "Leon"],
       "series": [
-        { "y": [247, 246, 241, 240], "type": "bar", "name": "TP" },
-        { "y": [319, 325, 335, 319], "type": "bar", "name": "GP" },
-        { "y": [0.77, 0.76, 0.72, 0.75], "type": "scatter", "name": "PPG" }
+          { "y": [247, 246, 241, 240], "type": "bar", "name": "TP" },
+          { "y": [319, 325, 335, 319], "type": "bar", "name": "GP" },
+          { "y": [0.77, 0.76, 0.72, 0.75], "type": "scatter", "name": "PPG" }
       ]
-    }
   }
+};
   
