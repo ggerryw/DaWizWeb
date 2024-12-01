8f9b25409d3b98a1611c39daf22c25cad9cb199a
ggerryw <104268807+ggerryw@users.noreply.github.com>
2024-12-01 08:48:24 -0500

    chart x title

--- JS/setupAndRender.js
+++ JS/setupAndRender.js
@@ -11,12 +11,11 @@ function renderChart(data) {
   const tpData = data.series.find((series) => series.name === 'TP').data;
   const gpData = data.series.find((series) => series.name === 'GP').data;
     // Calculate minimum and maximum for y-axis
-    const combinedMin = Math.min(...tpData, ...gpData);
-    const combinedMax = Math.max(...tpData, ...gpData);
-  
-    // Round down to the nearest decade for the minimum
-    const yAxisMin = Math.floor(combinedMin / 10) * 10;
-    const yAxisMax = Math.ceil(combinedMax / 10) * 10;
+  const combinedMin = Math.min(...tpData, ...gpData);
+  const combinedMax = Math.max(...tpData, ...gpData);
+  // Round down to the nearest decade for the minimum
+  const yAxisMin = Math.floor(combinedMin / 10) * 10;
+  const yAxisMax = Math.ceil(combinedMax / 10) * 10;
   const categories = data.categories;
 
   const tpGpRatio = tpData.map((value, index) =>
@@ -52,10 +51,10 @@ function renderChart(data) {
   const layout = {
     title: 'TP, GP, and PPG',
     xaxis: { title: 'Players', tickangle: -45 },
-    yaxis: { title: 'TP / GP' ,
+    yaxis: { title: 'Total Points and Games Played' ,
              range:[yAxisMin, yAxisMax],
     },
-    yaxis2: { title: 'PPG', overlaying: 'y', side: 'right' },
+    yaxis2: { title: 'Points Per Game', overlaying: 'y', side: 'right' },
     barmode: 'group',
     responsive: true,
   };
